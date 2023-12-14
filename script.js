96aaaa31ae1a01b223d1d054a5249677402cd070
William-Baaklini <76127831+William-Baaklini@users.noreply.github.com>
2023-12-14 23:35:41 +0200

    blob utf-8

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-document.getElementById("countButton").addEventListener("click", function () {
-  var text = document.getElementById("textInput").value;
+document.getElementById('countButton').addEventListener('click', function() {
+  var text = document.getElementById('textInput').value;
 
   // Remove punctuation and replace it with space
-  text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, " ");
-  text = text.replace(/\s{2,}/g, " ");
+  text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g," ");
+  text = text.replace(/\s{2,}/g," ");
 
-  var words = text.split(" ");
+  var words = text.split(' ');
   var wordCount = {};
 
-  words.forEach(function (word) {
-    if (word !== "") {
-      wordCount[word] = (wordCount[word] || 0) + 1;
-    }
+  words.forEach(function(word) {
+      if(word !== '') {
+          wordCount[word] = (wordCount[word] || 0) + 1;
+      }
   });
 
-  // Generate CSV string
-  var csvContent = "data:text/csv;charset=utf-8,Word,Count\n";
+  // Generate CSV content
+  var csvRows = ['Word,Count'];
   for (var word in wordCount) {
-    if (wordCount.hasOwnProperty(word)) {
-      csvContent += word + "," + wordCount[word] + "\n";
-    }
+      if (wordCount.hasOwnProperty(word)) {
+          csvRows.push(word + ',' + wordCount[word]);
+      }
   }
+  var csvString = csvRows.join('\n');
+
+  // Create a Blob for CSV data
+  var blob = new Blob(["\ufeff", csvString], { type: 'text/csv;charset=utf-8;' });
 
   // Create download link
-  var encodedUri = encodeURI(csvContent);
-  var link = document.getElementById("downloadLink");
-  link.setAttribute("href", encodedUri);
-  link.setAttribute("download", "word_count.csv");
-  link.style.display = "block";
-  link.textContent = "Download CSV";
+  var link = document.getElementById('downloadLink');
+  link.href = URL.createObjectURL(blob);
+  link.download = 'word_count.csv';
+  link.style.display = 'block';
+  link.textContent = 'Download CSV';
 
   // Display results
-  var resultDiv = document.getElementById("result");
+  var resultDiv = document.getElementById('result');
   resultDiv.innerHTML = JSON.stringify(wordCount, null, 2);
 });
+
