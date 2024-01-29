ec5cfe6a8a0e66ede5c40333a0919b4eb04228a6
William-Baaklini <76127831+William-Baaklini@users.noreply.github.com>
2024-01-29 13:09:40 +0200

    Sort Alphabetically, Table Styling

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
-document.getElementById('countButton').addEventListener('click', function() {
-  var text = document.getElementById('textInput').value;
+document.getElementById("countButton").addEventListener("click", function () {
+  var text = document.getElementById("textInput").value;
 
   // Remove punctuation and replace it with space
-  text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()«»]/g," ");
-  text = text.replace(/\s{2,}/g," ");
+  text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()«»]/g, " ");
+  text = text.replace(/\s{2,}/g, " ");
 
-  var words = text.split(' ');
+  var words = text.split(" ");
   var wordCount = {};
 
-  words.forEach(function(word) {
-      if(word !== '') {
-          wordCount[word] = (wordCount[word] || 0) + 1;
-      }
+  words.forEach(function (word) {
+    if (word !== "") {
+      wordCount[word] = (wordCount[word] || 0) + 1;
+    }
+  });
+
+  // Convert wordCount into an array of objects
+  var wordCountArray = Object.keys(wordCount).map(function (word) {
+    return { word: word, count: wordCount[word] };
+  });
+
+  // Sort the array alphabetically
+  wordCountArray.sort(function (a, b) {
+    return a.word.localeCompare(b.word);
   });
 
   // Create and display results in a table
-  var resultTable = document.getElementById('resultTable');
-  resultTable.innerHTML = createTable(wordCount);
+  var resultTable = document.getElementById("resultTable");
+  resultTable.innerHTML = createTable(wordCountArray);
 
   // Generate CSV content
-  var csvRows = ['Word,Count'];
-  for (var word in wordCount) {
-      if (wordCount.hasOwnProperty(word)) {
-          csvRows.push(word + ',' + wordCount[word]);
-      }
+  var csvRows = ["Word,Count"];
+  for (var i = 0; i < wordCountArray.length; i++) {
+    var wordObj = wordCountArray[i];
+    csvRows.push(wordObj.word + "," + wordObj.count);
   }
-  var csvString = csvRows.join('\n');
+  var csvString = csvRows.join("\n");
 
   // Create a Blob for CSV data
-  var blob = new Blob(["\ufeff", csvString], { type: 'text/csv;charset=utf-8;' });
+  var blob = new Blob(["\ufeff", csvString], {
+    type: "text/csv;charset=utf-8;",
+  });
 
   // Create download link
-  var link = document.getElementById('downloadLink');
+  var link = document.getElementById("downloadLink");
   link.href = URL.createObjectURL(blob);
-  link.download = 'word_count.csv';
-  link.style.display = 'block';
-  link.textContent = 'Download CSV';
-
-  // Display results
-  var resultDiv = document.getElementById('result');
-  resultDiv.innerHTML = JSON.stringify(wordCount, null, 2);
+  link.download = "word_count.csv";
+  link.style.display = "block";
+  link.textContent = "Download CSV";
 });
 
-function createTable(wordCount) {
-  var table = '<table><tr><th>Word</th><th>Count</th></tr>';
-  for (var word in wordCount) {
-      if (wordCount.hasOwnProperty(word)) {
-          table += '<tr><td>' + word + '</td><td>' + wordCount[word] + '</td></tr>';
-      }
+function createTable(wordCountArray) {
+  var table = "<table><tr><th>Word</th><th>Count</th></tr>";
+  for (var i = 0; i < wordCountArray.length; i++) {
+    var wordObj = wordCountArray[i];
+    table +=
+      "<tr><td>" + wordObj.word + "</td><td>" + wordObj.count + "</td></tr>";
   }
-  table += '</table>';
+  table += "</table>";
   return table;
 }
